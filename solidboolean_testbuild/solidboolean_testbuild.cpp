414ad822f5042713973d306ecab0416d3b8c3b6c
gubarev <gubarev.av@petrogm.com>
2024-07-31 15:37:53 +0700

    coords

--- solidboolean_testbuild/solidboolean_testbuild.cpp
+++ solidboolean_testbuild/solidboolean_testbuild.cpp
@@ -112,14 +112,55 @@ static bool loadObj(const std::string& filename,
 
 int main()
 {
-	std::vector<Vector3> firstVertices;
-	std::vector<std::vector<size_t>> firstTriangles;
+	std::vector<Vector3> firstVertices = 
+	{
+		{334.037841796875, 0, 381.257720947266},
+		{644.580139160156, 0, 1489.46728515625},
+		{1399.62426757813, 0, 1318.9736328125},
+		{334.037841796875, 450, 381.257720947266},
+		{644.580139160156, 450 ,1489.46728515625},
+		{1399.62426757813, 450, 1318.9736328125},
+	};
+	std::vector<std::vector<size_t>> firstTriangles =
+	{
+		{0 ,3, 2},
+		{0 ,2, 1},
+		{0, 4 ,3},
+		{0, 1 ,4},
+		{3, 5 ,2},
+		{4, 5, 3},
+		{2, 5, 1},
+		{1, 5 ,4},
 
-	std::vector<Vector3> secondVertices;
-	std::vector<std::vector<size_t>> secondTriangles;
+	};
 
-	loadObj("C:\\Users\\gubarev.av\\Desktop\\plotly\\obj3d\\mesh0.obj", firstVertices, firstTriangles);
-	loadObj("C:\\Users\\gubarev.av\\Desktop\\plotly\\obj3d\\mesh1.obj", secondVertices, secondTriangles);
+	std::vector<Vector3> secondVertices =
+	{
+		{0,0,0},
+		{850,0,0},
+		{850,200,0},
+		{0,200,0},
+		{0,0,850},
+		{850,0,850},
+		{850,200,850},
+		{0,200,850},
+	};
+	std::vector<std::vector<size_t>> secondTriangles =
+	{
+		{0,3,2},
+		{0,2,1},
+		{3,6,2},
+		{2,6,1},
+		{0,1,4},
+		{6,5,1},
+		{1,5,4},
+		{0,7,3},
+		{0,4,7},
+		{3,7,6},
+		{6,7,5},
+		{5,7,4},
+	};
+
 
 	auto t1 = std::chrono::high_resolution_clock::now();
 
